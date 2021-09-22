b1ca16a72d9460af078151bbe53f9354a57617b7
anonymous-PH1 <91187602+anonymous-PH1@users.noreply.github.com>
2021-09-22 19:52:31 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+
+
+function myFunc(){
+    document.body.style.background = "red";
+    document.getElementById('nav').style.background = "red";
+
+}
+
+
+var password;
+var pass1="Champion";
+
+password=prompt(' MAY PASSWORD TOH GAGO ');
+
+if (password==pass1)
+  alert('Password Correct! Click OK to enter!');
+else
+   {
+    alert('🖕🖕tang ina mo mali password mo🖕🖕');  
+    window.location="http://127.0.0.1:5500/index.html";
+    }
